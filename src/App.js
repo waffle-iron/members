39b784279a87997c1b9edb7393032c37720a73fb
Willy <willygdouglas@gmail.com>
2017-08-03 19:19:27 -0600

    Add members info from codefordenver.org/members

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MemberResources from './MemberResources';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <div className="App-header">
+      <h2>Code for Denver Members</h2>
+    </div>
+    <MemberResources />
+  </div>
+);
 
 export default App;
